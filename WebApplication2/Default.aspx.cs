516a60210e23a9cb6f5cff26084559f84d5f85dd
Sofiia Hlushko <sophie.glushko@ukr.net>
2018-06-15 13:02:28 +0300

    initial commit

---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                PlaceHolder1.Controls.Add(new Label { Text = i.ToString() });
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
